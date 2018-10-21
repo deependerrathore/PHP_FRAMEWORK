150eb9e745938dca07fe8d07eacb681de4523825
Deepender Rathore <deepenrathore28@gmail.com>
2018-10-21 21:12:03 +1100

    define path variables

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
-echo "PHP folder structure";
+define('DS', DIRECTORY_SEPARATOR); //Windows = \ , Linux,Mac = /
+define('ROOT',__FILE__); //Path of the index file, C:\Dev&Test\www\PHP_FRAMEWORK\index.php
+define('ROOT_DIR',__DIR__); //Root directory of the index file, C:\Dev&Test\www\PHP_FRAMEWORK
+
+echo DS . "<br>";
+echo ROOT . "<br>";
+echo ROOT_DIR ; 
+
+
+?>
