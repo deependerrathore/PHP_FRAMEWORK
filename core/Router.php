cee983c4476467b209e4d82cf4b9d9923a03402e
Deepender Rathore <deepenrathore28@gmail.com>
2018-10-23 00:45:59 +1100

    Extracting the controller name from

--- core/Router.php
+++ core/Router.php
@@ -1,8 +1,11 @@
 <?php
 
 class Router{
-    
-    public static function route($url){
-        dnd($url);
+
+    public static function route($url){//$url is defined in index.php
+        $controller = (!empty($url) && $url[0] != '') ? ucfirst($url[0]) : DEFAULT_CONTROLLER;//DEFAULT_CONTROLLER @config/config.php
+        $controller_name = $controller;
+        array_shift($url);
     }
+
 }
