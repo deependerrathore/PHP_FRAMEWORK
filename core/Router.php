f68fa686a319833ddea2dc3bdc91dd62825d1432
Deepender Rathore <deepenrathore28@gmail.com>
2018-10-23 00:51:10 +1100

    parameters stored in a variables

--- core/Router.php
+++ core/Router.php
@@ -3,14 +3,18 @@
 class Router{
 
     public static function route($url){//$url is defined in index.php
+        //controller
         $controller = (!empty($url) && $url[0] != '') ? ucfirst($url[0]) : DEFAULT_CONTROLLER;//DEFAULT_CONTROLLER @config/config.php
         $controller_name = $controller;
         array_shift($url);
 
+        //actions
         $action = (!empty($url) && $url[0] != '') ? lcfirst($url[0]) . 'Action' : 'indexAction';
         $action_name = $action;
         array_shift($url);
-        dnd($action);
+
+        //Params
+        $queryParams = $url;
     }
 
 }
