478ad8431c4b459253c4362c5c639d47b1de7aee
Deepender Rathore <deepenrathore28@gmail.com>
2018-10-25 23:51:07 +1100

    Adding views/home/index.php + default site title

--- config/config.php
+++ config/config.php
@@ -1,4 +1,10 @@
 <?php
 
 define('DEBUG',true);
-define('DEFAULT_CONTROLLER', 'Home');
+define('DEFAULT_CONTROLLER', 'Home'); // default controller if there isn't any contoller in url
+
+define('DeFAULT_LAYOUT','default'); // if no layout has ben set in contoller then use this layout
+
+define('PROJECT_ROOT','PHP_FRAMEWORK/');// set this to '/' for live server
+define('SITE_TITLE','MVC_FRAMEWORK'); ////this will be used if no site title will be used
+
