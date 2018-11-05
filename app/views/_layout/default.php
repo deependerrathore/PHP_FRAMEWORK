238858fc52c76dd26a45f52c30f1f57fb0aee4e9
Deepender Rathore <deepenrathore28@gmail.com>
2018-11-05 12:47:09 +1100

    Login form

--- app/views/_layout/default.php
+++ app/views/_layout/default.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="<?=PROJECT_ROOT;?>css/bootstrap.min.css">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"> 
     <link rel="stylesheet" href="<?=PROJECT_ROOT;?>css/custom.css">
     <script src="<?=PROJECT_ROOT;?>js/jquery-3.3.1.min.js"></script>
     <script src="<?=PROJECT_ROOT;?>js/bootstrap.min.js"></script>
---
+++ app/views/register/login.php
@@ -0,0 +1,39 @@
+<?php $this->setSiteTitle('Event Unacademy - Login');?>
+<?php $this->start('head'); ?>
+<?php $this->end();?>
+
+<?php $this->start('body');?>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="navbar-header">
+        <a href="#" class="navbar-brand">Event Unacademy</a>
+    </div>
+</nav>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 offset-md-3 well">
+                <h3 class="text-center">Login To Your Account</h3>
+                <p class="text-center">Get Unlimited tips,response questions<br/> free all the time</p>
+                <form action="<?=PROJECT_ROOT?>register/login" class="login" method="POST">
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <input type="text" class="form-control" id="username" name="username" placeholder="Enter Your Username">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                    </div>
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="remember_me" value="on">
+                        <label class="form-check-label" for="remember_me">Remember Me</label>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Login</button>
+                    <div class="text-right">
+                        <a class="text-primary" href="<?=PROJECT_ROOT?>/register/register">If not registered click here?</a>
+                    </div>
+                </form>
+        </div>
+    </div>    
+</div>
+<?php $this->end();?>
