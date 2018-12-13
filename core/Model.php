e5602811058acf08a869b5d6fe341f702c13f98e
Deepender Rathore <deepenrathore28@gmail.com>
2018-12-13 22:27:44 +1100

    updating Model class for softdelete search

--- core/Model.php
+++ core/Model.php
@@ -42,8 +42,24 @@ class Model{
     //     }
         
     // }
-
+    
+    protected function _softDeleteParams($params){
+        if ($this->_softDelete) {
+            if (array_key_exists('conditions',$params)) {
+                if (is_array($params['conditions'])) {
+                    $params['conditions'][] = "deleted != 1";
+                    
+                }else{
+                    $params['conditions'] .= ' AND deleted != 1';
+                }
+            }else{
+                $params['conditions'] = "deleted != 1";
+            }
+        }
+        return $params;
+    }
     public function find($params = []){
+        $params = $this->_softDeleteParams($params);
         $results = [];
         $resultsQuery = $this->_db->find($this->_table,$params);
         if ($resultsQuery) {
@@ -61,6 +77,7 @@ class Model{
     }
     
     public function findFirst($params = []){
+        $params = $this->_softDeleteParams($params);
         $resultQuery = $this->_db->findFirst($this->_table,$params);
         $result = new $this->_modelName($this->_table);
         if ($resultQuery) {
