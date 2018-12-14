5ff6565b42e7b9621f17c8df0b5c2566ed2a0054
Deepender Rathore <deepenrathore28@gmail.com>
2018-12-15 02:24:56 +1100

    form helper

--- app/libs/helpers/functions.php
+++ app/libs/helpers/functions.php
@@ -1,3 +1,4 @@
 <?php
 
 require_once(ROOT . DS . 'app' . DS . 'libs' . DS . 'helpers' . DS . 'helpers.php');
+require_once(ROOT . DS . 'app' . DS . 'libs' . DS . 'helpers' . DS . 'form_helpers.php');
--- app/views/tools/index.php
+++ app/views/tools/index.php
@@ -6,4 +6,7 @@
 
 <?php $this->start('body'); ?>
 <h1 class="text-center blue"> This is the tools index page</h1>
+<?= inputBlock('text','Username','Username','','Username',['class'=>'field'],['class'=>'label'],['class'=>'input is-primary'])?>
+
+<?= submitTag('Register',['class'=>'field is-grouped is-grouped-right'],['class'=>'control'],['class'=>'button']);?>
 <?php $this->end(); ?>
