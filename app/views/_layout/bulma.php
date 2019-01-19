a8d865a708c4a49b45bde96cfbebcf748b8409de
Deepender Rathore <deepenrathore28@gmail.com>
2019-01-19 13:26:11 +1100

    Uncaught ReferenceError: jQuery is not defined

--- app/views/_layout/bulma.php
+++ app/views/_layout/bulma.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="<?=PROJECT_ROOT;?>js/jquery-3.3.1.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css.map">
     <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous"> -->
@@ -17,7 +18,6 @@
     <div class="container-fluid" style="min-height:cal(100% - 125px);">
         <?=$this->content('body');?>
     </div>
-    <script src="<?=PROJECT_ROOT;?>js/jquery-3.3.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min.map"></script>
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
